11f9232057604109b37c63bba6c73c2e95d03ec0
Mạnh Tường Solo <41409442+vncloudsco@users.noreply.github.com>
2024-03-20 14:17:17 +0700

    arm update

--- src/lib/bindings/python/setup.py
+++ src/lib/bindings/python/setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
-#from distutils.core import setup
 import os
 import textwrap
 
@@ -8,25 +7,26 @@ current_dir = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
 with open(os.path.join(current_dir, "pyfaup", "__version__.py"), "r") as fp:
-          exec(fp.read(), about)
+    exec(fp.read(), about)
 
 setup(
     name='pyfaup',
-    version = about["__version__"],
-    description= about["__description__"],
-    author= about["__author__"],
-    author_email= about["__author_email__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
     long_description=textwrap.dedent(open("README.rst", "r").read()),
     long_description_content_type="text/x-rst",
     maintainer=about["__maintainer__"],
     maintainer_email=about["__maintainer_email__"],
     url=about["__url__"],
     license=about["__license__"],
-    packages = ["pyfaup"],
-    install_requires= ["chardet"],
-    package_dir = {"pyfaup": "pyfaup/"},
-    package_data = {'pyfaup': [
+    packages=["pyfaup"],
+    install_requires=["chardet"],
+    package_dir={"pyfaup": "pyfaup/"},
+    package_data={'pyfaup': [
         'Linux/x86_64/libfaupl.so',
+        'Linux/aarch64/libfaupl.so',
         'Darwin/x86_64/libfaupl.dylib',
         'Darwin/arm64/libfaupl.dylib',
         'mozilla.tlds'
